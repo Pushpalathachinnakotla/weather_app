c20c073e5e1489abcf4afc36086cb562a4068e82
1234Durga1234 <durgabhavanigosangi85@gmail.com>
2023-10-17 17:52:41 +0530

    weather_app

---
+++ weather/src/App.js
@@ -0,0 +1,10 @@
+import Header from "./components/Header"
+import Search from "./components/Search"
+export default function App() {
+  return (
+    <div>
+      <Header/>
+      <Search/>
+    </div>
+  )
+}
